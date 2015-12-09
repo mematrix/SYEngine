9ee10441e31f09d5ee1f4b59ea70f4e0d859b656
amamiya <rukino.saki@outlook.com>
2015-12-09 21:19:52 +0800

    old dexmuers projects deleted

--- MKVDemuxer/MKVDemuxer.Shared/Parser/Duration/OpusHelp.h
+++
@@ -1,9 +0,0 @@
-#ifndef _OPUS_HELP_H
-#define _OPUS_HELP_H
-
-int opus_packet_get_samples_per_frame(const unsigned char *data, int Fs);
-int opus_packet_get_nb_channels(const unsigned char *data);
-int opus_packet_get_nb_frames(const unsigned char packet[], int len);
-int opus_packet_get_nb_samples(const unsigned char packet[], int len, int Fs);
-
-#endif //_OPUS_HELP_H
