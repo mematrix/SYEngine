ae1e82ec5597b4ee1b0045c945e93da40d18708e
amamiya <rukino.saki@outlook.com>
2015-12-20 05:43:00 +0800

    merge ffmpeg lib

--- ffcodecs/ffcodecs.Shared/main.cpp
+++ ffcodecs/ffcodecs.Shared/main.cpp
@@ -1,1 +1,8 @@
-#include "ffmpeg.h"
+#include <Windows.h>
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
+{
+	if (ul_reason_for_call == DLL_PROCESS_ATTACH)
+		DisableThreadLibraryCalls(hModule);
+	return TRUE;
+}
