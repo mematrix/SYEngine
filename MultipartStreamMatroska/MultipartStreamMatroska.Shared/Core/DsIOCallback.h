5b6ecbb73dff18516d6b2e897cfe7a2e9fa1e46f
amamiya <rukino.saki@outlook.com>
2016-01-12 22:16:38 +0800

    add Network download reconnect notify and check statuscode

--- MultipartStreamMatroska/MultipartStreamMatroska.Shared/Core/DsIOCallback.h
+++ MultipartStreamMatroska/MultipartStreamMatroska.Shared/Core/DsIOCallback.h
@@ -4,24 +4,32 @@
 #include "MergeManager.h"
 #include "downloader/DownloadCore.h"
 #include <assert.h>
+#include <functional>
 
 class DsIOCallback : public MergeManager::IOCallback
 {
 	Downloader::Core::DataSource* _ds;
 	int64_t _cur_pos;
+
 	bool _reconnect; //http连接中断，尝试从新连接
+	std::function<void (int64_t,int64_t)>* _reconn_notify;
 
 public:
-	DsIOCallback() throw() : _ds(NULL), _cur_pos(0), _reconnect(false) {}
+	DsIOCallback() throw() : _ds(NULL), _cur_pos(0), _reconnect(false), _reconn_notify(NULL) {}
 	explicit DsIOCallback(Downloader::Core::DataSource* ds) throw() : _ds(ds), _cur_pos(0) {}
 	virtual ~DsIOCallback() throw() {}
 
+	inline Downloader::Core::DataSource* GetCurrentDs() throw()
+	{ return _ds; }
 	inline void SwapDs(Downloader::Core::DataSource* ds) throw()
 	{ _ds = ds; _cur_pos = 0; }
 	inline void ResetReadIo() throw()
 	{ Seek(0, SEEK_SET); }
+
 	inline void SetAutoReconnect(bool state = true) throw()
 	{ _reconnect = state; }
+	inline void SetReconnectNotify(std::function<void (int64_t,int64_t)>* callback) throw()
+	{ _reconn_notify = callback; }
 
 	virtual void* GetPrivate()
 	{ return _ds; }
@@ -29,7 +37,7 @@ public:
 	virtual int Read(void* buf, unsigned size)
 	{
 		unsigned rs = 0;
-		Downloader::Core::CommonResult r = _ds->ReadBytes(buf, size, &rs);
+		auto r = _ds->ReadBytes(buf, size, &rs);
 		if (r != Downloader::Core::CommonResult::kSuccess)
 			return 0;
 
@@ -41,6 +49,8 @@ public:
 				(_ds->GetFlags() & Downloader::Core::DataSource::Flags::kFromRemoteLink) != 0) {
 				if (_cur_pos < (total_size - 1)) {
 					//reconnect
+					if (_reconn_notify != NULL)
+						_reconn_notify->operator()(_cur_pos, total_size);
 					if (_ds->ReconnectAtOffset(_cur_pos) == Downloader::Core::CommonResult::kSuccess) {
 						_ds->ReadBytes(buf, size, &rs);
 						if (r != Downloader::Core::CommonResult::kSuccess)
