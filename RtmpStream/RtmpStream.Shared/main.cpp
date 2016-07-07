dcbb8046e648931f74602f3b099efa5e9c155ae7
xqq <xqq@xqq.im>
2016-07-08 02:41:13 +0800

    Add RTMP stream playback support (buggy)

---
+++ RtmpStream/RtmpStream.Shared/main.cpp
@@ -0,0 +1,37 @@
+#include <wrl\module.h>
+
+using namespace Microsoft::WRL;
+
+HMODULE khInstance;
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
+{
+    if (ul_reason_for_call == DLL_PROCESS_ATTACH) {
+        khInstance = hModule;
+        DisableThreadLibraryCalls(hModule);
+        Module<InProc>::GetModule().Create();
+    }
+    else if (ul_reason_for_call == DLL_PROCESS_DETACH) {
+        Module<InProc>::GetModule().Terminate();
+    }
+    return TRUE;
+}
+
+STDAPI DllGetActivationFactory(HSTRING activatibleClassId, IActivationFactory** factory)
+{
+#if WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_DESKTOP)
+    return E_NOTIMPL;
+#else
+    return Module<InProc>::GetModule().GetActivationFactory(activatibleClassId, factory);
+#endif
+}
+
+STDAPI DllGetClassObject(REFCLSID rclsid, REFIID riid, void** ppv)
+{
+    return Module<InProc>::GetModule().GetClassObject(rclsid, riid, ppv);
+}
+
+STDAPI DllCanUnloadNow()
+{
+    return Module<InProc>::GetModule().Terminate() ? S_OK : S_FALSE;
+}
